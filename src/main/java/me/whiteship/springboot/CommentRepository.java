20d33669abc86caa94fefddcda8681a9e55299a5
soonswan <swan.kim@oysterable.com>
2023-07-16 18:27:01 +0900

    [add] 사용자 전용 Repository interface 정의하기

---
+++ src/main/java/me/whiteship/springboot/CommentRepository.java
@@ -0,0 +1,9 @@
+package me.whiteship.springboot;
+
+import org.springframework.data.repository.RepositoryDefinition;
+
+import java.util.List;
+
+@RepositoryDefinition(domainClass = Comment.class, idClass = Long.class)
+public interface CommentRepository extends MyRepository<Comment, Long>{
+}
