a9e1bc676c04abe89793aa286f4db1515ec4ba8b
soonswan <swan.kim@oysterable.com>
2023-07-17 01:54:14 +0900

    [add] demojpa2 추가

---
+++ demojpa2/src/test/java/me/whiteship/demojpa2/Demojpa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.demojpa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demojpa2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
