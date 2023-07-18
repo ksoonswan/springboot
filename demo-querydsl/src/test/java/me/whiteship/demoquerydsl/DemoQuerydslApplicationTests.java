0e591f93dfd5e3200a7eff95b462fed89cf14d04
soonswan <swan.kim@oysterable.com>
2023-07-18 09:39:31 +0900

    [add] Querydsl 연동 보강

---
+++ demo-querydsl/src/test/java/me/whiteship/demoquerydsl/DemoQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.demoquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoQuerydslApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
