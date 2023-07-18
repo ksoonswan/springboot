2cf929866041df78bf5fb30a8c36de294661dc1a
soonswan <swan.kim@oysterable.com>
2023-07-18 12:07:47 +0900

    [add] commonweb 프로젝트 추가

---
+++ commonweb/src/test/java/me/whiteship/commonweb/CommonwebApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.commonweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonwebApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
