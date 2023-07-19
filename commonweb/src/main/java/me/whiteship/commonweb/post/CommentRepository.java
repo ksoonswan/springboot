56bfd5768afe00c83bc722e176e8e297b0558f5d
soonswan <swan.kim@oysterable.com>
2023-07-19 22:46:16 +0900

    [add] Jpa Transactional은 따로 공부할 필요가 있다.

--- commonweb/src/main/java/me/whiteship/commonweb/post/CommentRepository.java
+++ commonweb/src/main/java/me/whiteship/commonweb/post/CommentRepository.java
@@ -5,12 +5,14 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpecificationExecutor<Comment> {
 
   @EntityGraph(attributePaths = "post")
   Optional<Comment> getCommentById(Long id);
 
+  @Transactional(readOnly = true)
   <T> List<T> findByPost_Id(Long id, Class<T> type);
 
 
