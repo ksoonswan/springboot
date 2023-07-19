38cf29cf4e329cc7f41fad4250966a46af1d7cdc
soonswan <swan.kim@oysterable.com>
2023-07-19 10:39:53 +0900

    [add] jpa 쿼리 메소드

--- commonweb/src/main/java/me/whiteship/commonweb/post/PostRepository.java
+++ commonweb/src/main/java/me/whiteship/commonweb/post/PostRepository.java
@@ -1,7 +1,13 @@
 package me.whiteship.commonweb.post;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+  List<Post> findByTitleStartingWith(String title);
+
+  List<Post> findByTitle(String title);
+
 
 }
--- commonweb/src/test/java/me/whiteship/commonweb/PostControllerTest.java
+++ commonweb/src/test/java/me/whiteship/commonweb/PostControllerTest.java
@@ -29,23 +29,29 @@ public class PostControllerTest {
     post.setTitle("swanJpa");
     Post savePost = postRepository.save(post);// persist
 
-    assertThat(entityManager.contains(post)).isTrue();
-    assertThat(entityManager.contains(savePost)).isTrue();
-    assertThat(savePost == post).isTrue();
-
-    Post postUpdate = new Post();
-    postUpdate.setId(post.getId());
-    postUpdate.setTitle("hibernate");
-    Post updatedPost = postRepository.save(postUpdate);// merge
-
-    updatedPost.setTitle("whiteship");
-
-    assertThat(entityManager.contains(updatedPost)).isTrue();
-    assertThat(entityManager.contains(postUpdate)).isFalse();
-    assertThat(updatedPost == postUpdate).isFalse();
-
     List<Post> all = postRepository.findAll();
     assertThat(all.size()).isEqualTo(1);
+  }
 
+  @Test
+  public void findByTitleStartingWith(){
+    savePost();
+
+    List<Post> all = postRepository.findByTitleStartingWith("swan");
+    assertThat(all.size()).isEqualTo(1);
+  }
+
+  @Test
+  public void findByTitle(){
+    savePost();
+
+    List<Post> all = postRepository.findByTitle("swanJpa");
+    assertThat(all.size()).isEqualTo(1);
+  }
+
+  private void savePost(){
+    Post post = new Post();
+    post.setTitle("swanJpa");
+    postRepository.save(post);
   }
 }
