557bfacbf8010e8195a7d1b652f286e9c4016e28
soonswan <swan.kim@oysterable.com>
2023-07-19 10:08:31 +0900

    [add] jpaRepository.save()

--- commonweb/src/test/java/me/whiteship/commonweb/PostControllerTest.java
+++ commonweb/src/test/java/me/whiteship/commonweb/PostControllerTest.java
@@ -3,6 +3,7 @@ package me.whiteship.commonweb;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import jakarta.persistence.EntityManager;
 import java.util.List;
 import me.whiteship.commonweb.post.Post;
 import me.whiteship.commonweb.post.PostRepository;
@@ -19,13 +20,32 @@ public class PostControllerTest {
   @Autowired
   PostRepository postRepository;
 
+  @Autowired
+  private EntityManager entityManager;
+
   @Test
-  public void crud() throws Exception {
+  public void save(){
     Post post = new Post();
-    post.setTitle("jpa");
-    postRepository.save(post);
+    post.setTitle("swanJpa");
+    Post savePost = postRepository.save(post);// persist
+
+    assertThat(entityManager.contains(post)).isTrue();
+    assertThat(entityManager.contains(savePost)).isTrue();
+    assertThat(savePost == post).isTrue();
+
+    Post postUpdate = new Post();
+    postUpdate.setId(post.getId());
+    postUpdate.setTitle("hibernate");
+    Post updatedPost = postRepository.save(postUpdate);// merge
+
+    updatedPost.setTitle("whiteship");
+
+    assertThat(entityManager.contains(updatedPost)).isTrue();
+    assertThat(entityManager.contains(postUpdate)).isFalse();
+    assertThat(updatedPost == postUpdate).isFalse();
 
     List<Post> all = postRepository.findAll();
     assertThat(all.size()).isEqualTo(1);
+
   }
 }
