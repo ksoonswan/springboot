8a82a89de9f94cd21cff7d67eece3d4613d15dd7
soonswan <swan.kim@oysterable.com>
2023-07-19 10:56:45 +0900

    [add] Entity Named Param 황용

--- commonweb/src/main/java/me/whiteship/commonweb/post/PostRepository.java
+++ commonweb/src/main/java/me/whiteship/commonweb/post/PostRepository.java
@@ -4,13 +4,14 @@ import java.util.List;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
   List<Post> findByTitleStartingWith(String title);
 
-  @Query("SELECT p FROM Post AS p WHERE p.title = ?1")
-  List<Post> findByTitle(String title, Sort sort);
+  @Query("SELECT p FROM #{#entityName} AS p WHERE p.title = :title")
+  List<Post> findByTitle(@Param("title") String keyword, Sort sort);
 
 
 }
