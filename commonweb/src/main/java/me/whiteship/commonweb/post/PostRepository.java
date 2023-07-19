8d80aaa2ed1bc88a89a797f0bea543b3d6ac2d82
soonswan <swan.kim@oysterable.com>
2023-07-19 15:12:50 +0900

    [add] Update 쿼리

--- commonweb/src/main/java/me/whiteship/commonweb/post/PostRepository.java
+++ commonweb/src/main/java/me/whiteship/commonweb/post/PostRepository.java
@@ -1,8 +1,10 @@
 package me.whiteship.commonweb.post;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -13,5 +15,9 @@ public interface PostRepository extends JpaRepository<Post, Long> {
   @Query("SELECT p FROM #{#entityName} AS p WHERE p.title = :title")
   List<Post> findByTitle(@Param("title") String keyword, Sort sort);
 
+  @Modifying(clearAutomatically = true)
+  @Query("UPDATE Post p SET p.title =?1 WHERE p.id = ?2")
+  int updateTitle(String updateSuccess, Long id);
 
+  Optional<Post> findById(Long id);
 }
--- commonweb/src/test/java/me/whiteship/commonweb/PostControllerTest.java
+++ commonweb/src/test/java/me/whiteship/commonweb/PostControllerTest.java
@@ -4,6 +4,7 @@ package me.whiteship.commonweb;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Optional;
 import me.whiteship.commonweb.post.Post;
 import me.whiteship.commonweb.post.PostRepository;
 import org.junit.Test;
@@ -22,6 +23,12 @@ public class PostControllerTest {
 
   @Autowired
 //  private EntityManager entityManager;
+
+  private Post savePost(){
+    Post post = new Post();
+    post.setTitle("swanJpa");
+    return postRepository.save(post);
+  }
 
   @Test
   public void save(){
@@ -49,9 +56,24 @@ public class PostControllerTest {
     assertThat(all.size()).isEqualTo(1);
   }
 
-  private void savePost(){
-    Post post = new Post();
-    post.setTitle("swanJpa");
-    postRepository.save(post);
+  @Test
+  public void updateTitle(){
+    Post spring = savePost();
+
+    postRepository.updateTitle("updateSuccess", spring.getId());
+
+    // DB는 업데이트가 되었지만 캐시에는 남아있으므로, "updateSuccess" 변경이 안되고, "swanJpa" 그대로이다.
+    Optional<Post> byId = postRepository.findById(spring.getId());
+    assertThat(byId.get().getTitle()).isEqualTo("updateSuccess");
   }
+
+  @Test
+  public void updateTitleHibernate(){
+    Post spring = savePost();
+    spring.setTitle("hibernate");
+
+    List<Post> all = postRepository.findAll();
+    assertThat(all.get(1).getTitle()).isEqualTo("hibernate");
+  }
+
 }
